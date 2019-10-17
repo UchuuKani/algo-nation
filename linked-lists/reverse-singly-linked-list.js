02b4fe629f5300c95b3a9e1961881077844e518d
Alex Y <probesalesman@gmail.com>
2019-10-16 23:17:22 -0400

    adds reverse singly linked list soultion again

--- linked-lists/reverse-singly-linked-list.js
+++ linked-lists/reverse-singly-linked-list.js
@@ -1,8 +1,7 @@
 // https://www.hackerrank.com/challenges/reverse-a-linked-list
 
-// You’re given the pointer to the head node of a linked list. Change the next
-// pointers of the nodes so that their order is reversed. The head pointer given
-// may be null meaning that the initial list is empty.
+// You’re given the pointer to the head node of a linked list. Change the next pointers of the nodes
+// so that their order is reversed. The head pointer given may be null meaning that the initial list is empty.
 
 function reverse(head) {
   let current = head;
