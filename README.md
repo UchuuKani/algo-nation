75d4e9cc0a940c0649d004f78237fde658119150
Alex Y <probesalesman@gmail.com>
2020-02-05 18:09:40 -0500

    adds solution to maximum-depth-of-binary-tree

--- README.md
+++ README.md
@@ -2,4 +2,11 @@
 
 ## This is a list of all the algorithms I will be doing in the coming months
 
-I will categorize by type and explain my solutions
+### Current Categories Include
+
+- Array and String Manipulation
+- Binary Search Trees
+- Generic Trees and Graphs
+- Dynamic Programming
+- Linked Lists
+- Some SQL queries from LeetCode
--- trees-and-graphs/maximum-depth-binary-tree.js
+++ trees-and-graphs/maximum-depth-binary-tree.js
@@ -1,5 +1,23 @@
 // input is tree of nodes, rather than an array
 
-function maxDepth(root) {
-  //
+function maxDepth(root, prevVal = 0) {
+  if (!root) {
+    return 0;
+  }
+  if (!root.left && !root.right) {
+    return prevVal + 1;
+  }
+
+  if (root.left || root.right) {
+    const leftVal = maxDepth(root.left, prevVal + 1);
+    const rightVal = maxDepth(root.right, prevVal + 1);
+
+    return Math.max(leftVal, rightVal);
+  }
 }
+
+// time complexity: O(n) where n is number of nodes in tree. We're doing DFS to visit the deepest node in the tree, and have to
+// visit all n nodes to make sure that leaves are being hit
+
+// space complexity: O(h) where h is max depth of tree. At worst, might be O(n) if the tree is effectively a linked list but
+// space is closer to h the closer the tree is to being balanced
